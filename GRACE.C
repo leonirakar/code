57942234d81374023dbbfc94919b19f6519bdc4c
Nirakar Phuyal <leonirakar@users.noreply.github.com>
2016-07-13 10:30:49 +0545

    Add files via upload

---
+++ GRACE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void addgracemarks(int *n){
+//*code
+}
+int main(void){
+	int marks;
+	printf("Enter the actuak marks/n");
+	scanf("%d",&marks);
+	//*code
+
+	printf("/nThe grace marks is: %d\n",marks);
+	}
